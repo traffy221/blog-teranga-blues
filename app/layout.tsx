6ddd93e500f4f4866035708556396cc16cbce91b
Traffy Law <princebladii97@gmail.com>
2025-12-15 11:34:22 +0000

    Add Netlify Identity Widget to main layout for invite flow

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import CommandPalette from "@/components/CommandPalette";
 
@@ -36,11 +37,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        {/* Netlify Identity Widget - Required for admin authentication */}
+        <Script
+          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         className={`${playfair.variable} ${inter.variable} font-sans antialiased`}
       >
         {children}
         <CommandPalette />
+
+        {/* Netlify Identity redirect script */}
+        <Script id="netlify-identity-redirect" strategy="afterInteractive">
+          {`
+            if (window.netlifyIdentity) {
+              window.netlifyIdentity.on("init", user => {
+                if (!user) {
+                  window.netlifyIdentity.on("login", () => {
+                    document.location.href = "/admin/";
+                  });
+                }
+              });
+            }
+          `}
+        </Script>
       </body>
     </html>
   );
